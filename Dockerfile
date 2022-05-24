38f66bfddb7796c512d3b8e8fc1581bd9abb82b2
MustakimK <mustakimkazi10@gmail.com>
2022-05-24 01:24:25 +0000

    enviroment variable changes

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,11 @@
 FROM python:3.10
 
-WORKDIR /scripts
-
 ADD run.py /
 
 # RUN pip install os
 # RUN pip install 
 
-CMD ["python3", "./run.py"]
+CMD ["python3", "-u" , "./run.py"]
 
 # Take folder X, zip it up, put it in backups folder with timestamp every 5 minutes, Delete any backups older than a day, Backup that folder to NFS
 # Need crontab too
--- run.py
+++ run.py
@@ -1,27 +1,34 @@
+from email.policy import default
 from fileinput import filename
 import os,time
 from datetime import datetime
 import shutil
 
 def main():
-    while(True):
-        backup_frequency = os.environ['BACKUP_FREQUENCY', 60]
-        backup_data()
-        delete_old_files()  
-        time.sleep(backup_frequency*60)
-
-def backup_data():
-
-    fileName = datetime.now().strftime("%Y_%m_%d-%H_%M_%S_%p")
+    backup_frequency = os.getenv('BACKUP_FREQUENCY', '60')
+    backup_age = os.getenv('BACKUP_AGE', '1')
+    
     source = "/saves"
     destination= "/backups"
 
+    while(True):
+        
+        fileName = datetime.now().strftime("%Y_%m_%d-%H_%M_%S_%p")
+
+        backup_data(destination, fileName, source)
+        delete_old_files(backup_age, destination)  
+        time.sleep(int(backup_frequency)*60)
+
+def backup_data(destination, fileName, source):
+
     shutil.make_archive(base_name=(os.path.join(destination,fileName)), format="zip", root_dir=source)
 
-def delete_old_files():
-    backup_age = os.environ['BACKUP_AGE',1]
-    destination= "/home/kaz/DockerFiles/backups"
-           
+def delete_old_files(backup_age, destination):
+    backup_age = os.getenv('BACKUP_AGE', '1')
+    destination= "/backups"
+    os.system("find " + destination + " -mtime +60 -print")
+    # get todays date, parse all the info
+
     # files_list = os.listdir(destination)
     # current_time = time.time()
     # for backup_files in files_list:
