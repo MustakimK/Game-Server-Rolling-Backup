8de0e580ed9789add2ef2f3b9471dd06c61f3170
MustakimK <mustakimkazi10@gmail.com>
2022-05-25 16:05:17 -0400

    added constants for variables and changed variable names to make them more clear

--- run.py
+++ run.py
@@ -3,35 +3,38 @@ from datetime import datetime
 import shutil
 
 def main():
-    backup_frequency = os.getenv('BACKUP_FREQUENCY', '60')
-    backup_age = os.getenv('BACKUP_AGE', '1')
-    source = os.getenv('VOL_SRC', '/saves')
-    destination= os.getenv('VOL_DST', '/backups')
+    BACKUP_FREQUENCY_CONSTANT = '60'
+    BACKUP_AGE_CONSTANT = '1'
+    SOURCE_DIR_CONSTANT = '/saves'
+    DESTINATION_DIR_CONSTANT = '/backups'
+
+    backup_frequency = os.getenv('BACKUP_FREQUENCY', BACKUP_FREQUENCY_CONSTANT)
+    backup_age = os.getenv('BACKUP_AGE', BACKUP_AGE_CONSTANT)
+    source_dir = os.getenv('SAVE_DIR', SOURCE_DIR_CONSTANT)
+    destination_dir = os.getenv('BACKUP_DIR', DESTINATION_DIR_CONSTANT)
 
     while(True):
         
         fileName = datetime.now().strftime("%Y_%m_%d-%H_%M_%S_%p")
 
-        backup_data(destination, fileName, source)
-        delete_old_files(backup_age, destination)  
+        backup_data(destination_dir, fileName, source_dir)
+        delete_old_files(backup_age, destination_dir)
         time.sleep(int(backup_frequency)*60)
 
-def backup_data(destination, fileName, source):
-    shutil.make_archive(base_name=(os.path.join(destination,fileName)), format="zip", root_dir=source)
+def backup_data(destination_dir, fileName, source_dir):
+    shutil.make_archive(base_name=(os.path.join(destination_dir,fileName)), format="zip", root_dir=source_dir)
 
-def delete_old_files(backup_age, destination):
+def delete_old_files(backup_age, destination_dir):
 
-    list_of_files = os.listdir(destination)
+    list_of_files = os.listdir(destination_dir)
 
     for filename in list_of_files:
 
-        path = os.path.join(destination, filename)
+        path = os.path.join(destination_dir, filename)
+        modified_time = os.path.getmtime(path)
 
-        modified_time=os.path.getmtime(path)
         if time.time()-modified_time > (backup_age * 86400): #time in seconds
             os.remove(path)
             
 if __name__=="__main__":
     main()
-
-#unit tests, support units, make constants 
