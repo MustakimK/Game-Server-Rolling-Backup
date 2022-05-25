9c112a9aa73250551857a76e84806706d530e78b
MustakimK <mustakimkazi10@gmail.com>
2022-05-24 23:44:37 -0400

    completed delete_old_files function and updated docker-compose.yml to use enviroment variables for SRC and DST locations

--- run.py
+++ run.py
@@ -5,7 +5,6 @@ import shutil
 def main():
     backup_frequency = os.getenv('BACKUP_FREQUENCY', '60')
     backup_age = os.getenv('BACKUP_AGE', '1')
-    
     source = os.getenv('VOL_SRC', '/saves')
     destination= os.getenv('VOL_DST', '/backups')
 
@@ -18,28 +17,20 @@ def main():
         time.sleep(int(backup_frequency)*60)
 
 def backup_data(destination, fileName, source):
-
     shutil.make_archive(base_name=(os.path.join(destination,fileName)), format="zip", root_dir=source)
 
 def delete_old_files(backup_age, destination):
-    # os.system("find " + destination + " -mtime +60 -print")
-    # get todays date, parse all the info
 
     list_of_files = os.listdir(destination)
-    print(list_of_files)
-    for filename in list_of_files:
-        modified_time=os.path.getmtime((os.path.join(destination, filename)))
-        if time.time()-modified_time > 120: #time in seconds
-            os.remove(os.path.join(destination, filename))
-            
-    # files_list = os.listdir(destination)
-    # current_time = time.time()
-    # for backup_files in files_list:
-    #     file_path = os.path.join(destination, backup_files)
-    #     if os.path.isfile(file_path):
-    #         if (current_time - os.stat(file_path).st_birthtime) > backup_age * 86400:
-    #             os.remove(file_path)
 
+    for filename in list_of_files:
+
+        path = os.path.join(destination, filename)
+
+        modified_time=os.path.getmtime(path)
+        if time.time()-modified_time > (backup_age * 86400): #time in seconds
+            os.remove(path)
+            
 if __name__=="__main__":
     main()
 
