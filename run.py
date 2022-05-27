08be91ac6ec9808533e83583a762ea144192be8f
MustakimK <mustakimkazi10@gmail.com>
2022-05-27 18:10:31 -0400

    Added support for time units

--- run.py
+++ run.py
@@ -9,9 +9,14 @@ def main():
     DESTINATION_DIR_CONSTANT = '/backups'
 
     backup_frequency = os.getenv('BACKUP_FREQUENCY', BACKUP_FREQUENCY_CONSTANT)
-    backup_age = os.getenv('BACKUP_AGE', BACKUP_AGE_CONSTANT)
+    backup_age = os.getenv('OLDEST_BACKUP_AGE', BACKUP_AGE_CONSTANT)
     source_dir = os.getenv('SAVE_DIR', SOURCE_DIR_CONSTANT)
     destination_dir = os.getenv('BACKUP_DIR', DESTINATION_DIR_CONSTANT)
+
+    backup_frequency = convert_to_seconds(backup_frequency)
+    backup_age = convert_to_seconds(backup_age)
+
+    print(backup_age,backup_frequency)
 
     while(True):
         
@@ -19,7 +24,7 @@ def main():
 
         backup_data(destination_dir, fileName, source_dir)
         delete_old_files(backup_age, destination_dir)
-        time.sleep(int(backup_frequency)*60)
+        time.sleep(backup_frequency)
 
 def backup_data(destination_dir, fileName, source_dir):
     shutil.make_archive(base_name=(os.path.join(destination_dir,fileName)), format="zip", root_dir=source_dir)
@@ -33,8 +38,13 @@ def delete_old_files(backup_age, destination_dir):
         path = os.path.join(destination_dir, filename)
         modified_time = os.path.getmtime(path)
 
-        if time.time()-modified_time > (backup_age * 86400): #time in seconds
+        if time.time()-modified_time > (backup_age): #* 86400): #time in seconds
             os.remove(path)
-            
+
+def convert_to_seconds(time_string):
+    units_in_seconds = {'m': 2628000, 'w': 604800, 'd': 86400, 'h': 3600, 'm': 60, 's': 1}
+
+    return (int(time_string[:-1]) * (units_in_seconds[time_string[-1].lower()]))
+
 if __name__=="__main__":
     main()
